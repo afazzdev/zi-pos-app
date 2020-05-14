4b3d6105aa14f3072646fb549250def859f21a50
afazzdev <afaizi1999@gmail.com>
2020-05-14 23:17:08 +0700

    Create /dashboard

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,12 +1,22 @@
 import React from 'react';
-import { Route, Switch } from 'react-router-dom';
+import { Route, Switch, Link } from 'react-router-dom';
 import Login from '../pages/Login';
+import Dashboard from '../pages/Dashboard';
 
 const Routes = () => {
   return (
     <>
       <Switch>
-        <Route exact path='/' component={() => <div>Home</div>} />
+        <Route
+          exact
+          path='/'
+          component={() => (
+            <div>
+              <Link to='/dashboard'>Dashboard</Link>>
+            </div>
+          )}
+        />
+        <Route path='/dashboard' component={Dashboard} />
         <Route path='/login' component={Login} />
       </Switch>
     </>
